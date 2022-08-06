ef15aa48b1016c9ed28e3f74fecc46e869529bf7
Josue Munganga <39856434+josumung999@users.noreply.github.com>
2022-08-06 10:55:00 +0200

    Built a gql server

---
+++ src/index.ts
@@ -0,0 +1,14 @@
+import { ApolloServer } from "apollo-server";
+
+// 1
+import { schema } from './schema';
+export const server = new ApolloServer({
+  schema,
+});
+
+const port = 4000;
+
+// 2
+server.listen({port}).then(({ url }) => {
+  console.log(`Server ready at ${url}`);
+});
