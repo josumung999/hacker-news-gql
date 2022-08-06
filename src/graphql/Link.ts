198e7dd81a717aba78c752117ae04bfc8b7e2b64
Josue Munganga <39856434+josumung999@users.noreply.github.com>
2022-08-06 11:21:30 +0200

    generating types with nexus

---
+++ src/graphql/Link.ts
@@ -0,0 +1,10 @@
+import { objectType } from "nexus";
+
+export const Link = objectType({
+    name: "Link", // 1 
+    definition(t) {  // 2
+        t.nonNull.int("id"); // 3 
+        t.nonNull.string("description"); // 4
+        t.nonNull.string("url"); // 5 
+    },
+});
